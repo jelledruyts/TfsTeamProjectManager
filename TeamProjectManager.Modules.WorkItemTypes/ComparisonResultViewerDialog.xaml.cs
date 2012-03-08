8320f4fa516ade6dce38a32af094a50d95f147a5
Jelle Druyts <jelle_druyts@yahoo.com>
2012-03-08 09:49:06 +0000

    Added support for Visual Studio 11 Diff Tool and for custom Diff Tools

--- TeamProjectManager.Modules.WorkItemTypes/ComparisonResultViewerDialog.xaml.cs
+++ TeamProjectManager.Modules.WorkItemTypes/ComparisonResultViewerDialog.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Windows;
@@ -11,7 +13,7 @@ namespace TeamProjectManager.Modules.WorkItemTypes
     public partial class ComparisonResultViewerDialog : Window
     {
         public TeamProjectComparisonResult Comparison { get; private set; }
-        private string diffMergeFileName;
+        private DiffTool diffTool;
 
         public ComparisonResultViewerDialog(TeamProjectComparisonResult comparison)
         {
@@ -19,7 +21,8 @@ namespace TeamProjectManager.Modules.WorkItemTypes
             this.Comparison = comparison;
             this.DataContext = this.Comparison;
             this.Title = string.Format(CultureInfo.CurrentCulture, "Comparison result details for Team Project \"{0}\"", this.Comparison.TeamProject);
-            this.diffMergeFileName = GetDiffMergeFileName();
+            this.diffTool = GetDiffTool();
+            this.infoLabel.Text = this.diffTool == null ? "No Diff Tool Detected" : string.Format(CultureInfo.CurrentCulture, "Using {0} Diff Tool", this.diffTool.Name);
         }
 
         private void okButton_Click(object sender, RoutedEventArgs e)
@@ -41,7 +44,7 @@ namespace TeamProjectManager.Modules.WorkItemTypes
 
         private bool CanCompareInDiffTool(WorkItemTypeComparisonResult workItemTypeComparison)
         {
-            return this.diffMergeFileName != null && this.sourceResultsDataGrid.SelectedItem != null && workItemTypeComparison != null && (workItemTypeComparison.Status == ComparisonStatus.AreDifferent || workItemTypeComparison.Status == ComparisonStatus.AreEqual);
+            return this.diffTool != null && this.sourceResultsDataGrid.SelectedItem != null && workItemTypeComparison != null && (workItemTypeComparison.Status == ComparisonStatus.AreDifferent || workItemTypeComparison.Status == ComparisonStatus.AreEqual);
         }
 
         private void workItemTypeResultsDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -72,9 +75,7 @@ namespace TeamProjectManager.Modules.WorkItemTypes
                         var sourceLabel = string.Format(CultureInfo.CurrentCulture, "Work Item Type '{0}' in Source '{1}'", workItemTypeComparison.WorkItemTypeName, sourceResult.Source.Name);
                         var targetLabel = string.Format(CultureInfo.CurrentCulture, "Work Item Type '{0}' in Team Project '{1}'", workItemTypeComparison.WorkItemTypeName, this.Comparison.TeamProject);
 
-                        var processInfo = new ProcessStartInfo(this.diffMergeFileName, string.Format(CultureInfo.InvariantCulture, "\"{0}\" \"{1}\" \"{2}\" \"{3}\" /ignorespace", sourceFile, targetFile, sourceLabel, targetLabel));
-                        var process = Process.Start(processInfo);
-                        process.WaitForExit();
+                        this.diffTool.Launch(sourceFile, targetFile, sourceLabel, targetLabel);
                     }
                     finally
                     {
@@ -85,37 +86,94 @@ namespace TeamProjectManager.Modules.WorkItemTypes
             }
         }
 
-        private static string GetDiffMergeFileName()
+        private static DiffTool GetDiffTool()
         {
-            var idePath = GetVisualStudioIdePath();
-            if (idePath != null)
+            var diffToolCommandOverride = ConfigurationManager.AppSettings["DiffToolCommand"];
+            var diffToolArgumentsOverride = ConfigurationManager.AppSettings["DiffToolArguments"];
+            if (!string.IsNullOrEmpty(diffToolCommandOverride))
             {
-                var diffMergeFileName = Path.Combine(idePath, "diffmerge.exe");
-                if (File.Exists(diffMergeFileName))
+                return new DiffTool("Custom", diffToolCommandOverride, diffToolArgumentsOverride);
+            }
+            else
+            {
+                return GetVisualStudioDiffTool();
+            }
+        }
+
+        private static DiffTool GetVisualStudioDiffTool()
+        {
+            // Visual Studio 11 has a built-in diff tool, call devenv.exe directly.
+            using (var regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\11.0\Setup\VS"))
+            {
+                if (regKey != null)
                 {
-                    return diffMergeFileName;
+                    var devenvPath = (string)regKey.GetValue("EnvironmentPath");
+                    if (File.Exists(devenvPath))
+                    {
+                        return new DiffTool("Visual Studio 11", devenvPath, "/diff %1 %2 %6 %7");
+                    }
+                }
+            }
+
+            // Older versions should have a diffmerge.exe tool in the IDE path.
+            string idePath = null;
+            string diffToolName = null;
+            using (var regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\10.0\Setup\VS"))
+            {
+                if (regKey != null)
+                {
+                    idePath = (string)regKey.GetValue("EnvironmentDirectory");
+                    diffToolName = "Visual Studio 2010";
+                }
+            }
+            if (!Directory.Exists(idePath))
+            {
+                using (var regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\9.0\Setup\VS"))
+                {
+                    if (regKey != null)
+                    {
+                        idePath = (string)regKey.GetValue("EnvironmentDirectory");
+                        diffToolName = "Visual Studio 2008";
+                    }
+                }
+            }
+            if (Directory.Exists(idePath))
+            {
+                var diffmergePath = Path.Combine(idePath, "diffmerge.exe");
+                if (File.Exists(diffmergePath))
+                {
+                    return new DiffTool(diffToolName, diffmergePath, "%1 %2 %6 %7 /ignorespace");
                 }
             }
             return null;
         }
 
-        private static string GetVisualStudioIdePath()
+        private class DiffTool
         {
-            using (var regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\10.0\Setup\VS"))
+            public string Name { get; private set; }
+            public string Command { get; private set; }
+            public string Arguments { get; private set; }
+
+            public DiffTool(string name, string command, string arguments)
             {
-                if (regKey != null)
-                {
-                    return (string)regKey.GetValue("EnvironmentDirectory");
-                }
+                this.Name = name;
+                this.Command = command;
+                this.Arguments = arguments;
             }
-            using (var regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\9.0\Setup\VS"))
+
+            public void Launch(string sourceFileName, string targetFileName, string sourceLabel, string targetLabel)
             {
-                if (regKey != null)
-                {
-                    return (string)regKey.GetValue("EnvironmentDirectory");
-                }
+                var command = Environment.ExpandEnvironmentVariables(this.Command);
+                var arguments = this.Arguments;
+                arguments = arguments.Replace("%1", string.Concat("\"", sourceFileName, "\""));
+                arguments = arguments.Replace("%2", string.Concat("\"", targetFileName, "\""));
+                arguments = arguments.Replace("%6", string.Concat("\"", sourceLabel, "\""));
+                arguments = arguments.Replace("%7", string.Concat("\"", targetLabel, "\""));
+                arguments = Environment.ExpandEnvironmentVariables(arguments);
+                var processInfo = new ProcessStartInfo(command, arguments);
+                var process = Process.Start(processInfo);
+                process.WaitForExit();
             }
-            return null;
         }
     }
 }

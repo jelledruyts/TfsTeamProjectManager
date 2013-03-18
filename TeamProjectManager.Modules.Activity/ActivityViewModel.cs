53c12630e6890b550adc46dd4f7b46d2e5f5b8ea
Jelle Druyts <jelle_druyts@yahoo.com>
2013-03-18 13:09:27 +0000

    Fixed work item tracking issue in activity module, small other changes, added screenshots for activity module documentation

--- TeamProjectManager.Modules.Activity/ActivityViewModel.cs
+++ TeamProjectManager.Modules.Activity/ActivityViewModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using Microsoft.Practices.Prism.Events;
+using Microsoft.TeamFoundation.Build.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Globalization;
 using System.Linq;
 using System.Windows;
-using Microsoft.Practices.Prism.Events;
-using Microsoft.TeamFoundation.Build.Client;
-using Microsoft.TeamFoundation.VersionControl.Client;
-using Microsoft.TeamFoundation.WorkItemTracking.Client;
 using TeamProjectManager.Common;
 using TeamProjectManager.Common.Events;
 using TeamProjectManager.Common.Infrastructure;
@@ -139,7 +139,7 @@ namespace TeamProjectManager.Modules.Activity
                         var parameters = new Dictionary<string, object>() {
                                 { "TeamProject", teamProject.Name}
                             };
-                        var workItemsQuery = wit.Query("SELECT [System.Id], [System.ChangedDate], [System.Title] FROM WorkItems WHERE [System.TeamProject] = @TeamProject ORDER BY [System.ChangedDate]", parameters);
+                        var workItemsQuery = wit.Query("SELECT [System.Id], [System.ChangedDate], [System.Title] FROM WorkItems WHERE [System.TeamProject] = @TeamProject ORDER BY [System.ChangedDate] DESC", parameters);
                         var workItems = workItemsQuery.Cast<WorkItem>().Take(numberOfActivities).ToList();
                         var workItemActivities = workItems.Select(w => new ComponentActivityInfo("Work Item Tracking", w.ChangedDate, string.Format(CultureInfo.CurrentCulture, "{0} {1}: \"{2}\"", w.Type.Name, w.Id, w.Title))).ToList();
 
@@ -150,7 +150,7 @@ namespace TeamProjectManager.Modules.Activity
                         spec.MaxBuildsPerDefinition = numberOfActivities;
                         spec.QueryDeletedOption = QueryDeletedOption.IncludeDeleted;
                         var builds = buildServer.QueryBuilds(spec).Builds;
-                        var teamBuildActivities = builds.OrderByDescending(b => b.StartTime).Take(numberOfActivities).Select(b => new ComponentActivityInfo("Team Build", b.StartTime, string.Format(CultureInfo.CurrentCulture, "Build Number \"{0}\" from Build Definition \"{1}\"", b.BuildDefinition.Name, b.BuildNumber)));
+                        var teamBuildActivities = builds.OrderByDescending(b => b.StartTime).Take(numberOfActivities).Select(b => new ComponentActivityInfo("Team Build", b.StartTime, string.Format(CultureInfo.CurrentCulture, "Build Number \"{0}\" from Build Definition \"{1}\"", b.BuildNumber, b.BuildDefinition.Name)));
 
                         // Assemble the complete activity details.
                         activity.Add(new TeamProjectActivityInfo(teamProject.Name, numberOfActivities, sourceControlActivities, workItemActivities, teamBuildActivities));

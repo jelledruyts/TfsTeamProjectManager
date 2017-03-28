7ab136d56b91cd6a222e71b0c97f04d7767ad501
Jelle Druyts <jelle_druyts@yahoo.com>
2017-03-28 21:06:29 +0200

    Added triggers and schedules columns for build definitions. Fixes #16.

---
+++ TeamProjectManager.Modules.BuildAndRelease/BuildDefinitionTriggerSchedulesConverter.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.TeamFoundation.Build.WebApi;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace TeamProjectManager.Modules.BuildAndRelease
+{
+    public class BuildDefinitionTriggerSchedulesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var buildDefinition = value as BuildDefinition;
+            if (buildDefinition == null)
+            {
+                return null;
+            }
+            var schedules = new StringBuilder();
+            foreach (var schedule in buildDefinition.Triggers.OfType<ScheduleTrigger>().SelectMany(t => t.Schedules).Where(s => s.DaysToBuild != ScheduleDays.None))
+            {
+                if (schedules.Length > 0)
+                {
+                    schedules.Append("; ");
+                }
+                var firstSchedule = true;
+                foreach (var scheduleDay in Enum.GetValues(typeof(ScheduleDays)).Cast<ScheduleDays>().Where(s => s != ScheduleDays.All && s != ScheduleDays.None))
+                {
+                    if (schedule.DaysToBuild.HasFlag(scheduleDay))
+                    {
+                        if (!firstSchedule)
+                        {
+                            schedules.Append(", ");
+                        }
+                        firstSchedule = false;
+                        schedules.Append(scheduleDay.ToString());
+                    }
+                }
+                schedules.AppendFormat(culture, " at {0}:{1:00}", schedule.StartHours, schedule.StartMinutes);
+            }
+            return schedules.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeamProjectManager.Modules.BuildAndRelease/BuildDefinitionTriggerTypesConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.TeamFoundation.Build.WebApi;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace TeamProjectManager.Modules.BuildAndRelease
+{
+    public class BuildDefinitionTriggerTypesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var buildDefinition = value as BuildDefinition;
+            if (buildDefinition == null)
+            {
+                return null;
+            }
+            var triggerTypes = buildDefinition.Triggers.Select(t => t.TriggerType.ToString()).Distinct().OrderBy(t => t);
+            return string.Join(", ", triggerTypes);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

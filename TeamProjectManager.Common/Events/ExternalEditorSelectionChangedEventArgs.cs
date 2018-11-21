26b9e92c73340d2f277490b44a7a05e7b876b226
jodli <job87@web.de>
2018-11-21 12:27:02 +0100

    Added new selection changed event and eventargs for the external editor.

---
+++ TeamProjectManager.Common/Events/ExternalEditorSelectionChangedEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeamProjectManager.Common.Events
+{
+    /// <summary>
+    /// Defines the event arguments for External Editor selection change events.
+    /// </summary>
+    public class ExternalEditorSelectionChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the External Editor that has been selected.
+        /// </summary>
+        public string SelectedExternalEditor { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExternalEditorSelectionChangedEventArgs"/> class.
+        /// </summary>
+        /// <param name="selectedExternalEditor">The External Editor that has been selected.</param>
+        public ExternalEditorSelectionChangedEventArgs(string selectedExternalEditor)
+        {
+            this.SelectedExternalEditor = selectedExternalEditor;
+        }
+    }
+}

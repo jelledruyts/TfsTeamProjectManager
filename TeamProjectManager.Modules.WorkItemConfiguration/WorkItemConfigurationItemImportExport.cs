77447bc4231adc5160dbf19b5688b87b7a97ffba
Andre Mainka <Andre.Mainka@siemens.com>
2021-07-29 14:04:38 +0200

    more cleanups and packagereferences

--- TeamProjectManager.Modules.WorkItemConfiguration/WorkItemConfigurationItemImportExport.cs
+++ TeamProjectManager.Modules.WorkItemConfiguration/WorkItemConfigurationItemImportExport.cs
@@ -7,9 +7,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
-using System.Web.Services.Protocols;
 using System.Xml;
 using System.Xml.Schema;
 using TeamProjectManager.Common;
@@ -481,7 +479,7 @@ namespace TeamProjectManager.Modules.WorkItemConfiguration
                 // Working with the ProjectProcessConfigurationService throws NullReferenceException on TFS 2010 or earlier.
                 return null;
             }
-            catch (SoapException)
+            catch (SystemException)
             {
                 // A Team Project that doesn't have a process configuration yet throws a SOAP exception
                 // "TF400572: The Project Process Settings must be configured for this feature to be used".
